6ea910ff7d875205692d5b9df5f71d6764340644
sebsibe23 <sebsibe1990@proton.me>
2024-06-06 12:13:33 -0700

    fix models/user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -5,12 +5,21 @@ from models.base import Base
 
 
 class UserSession(Base):
-    """User session class.
+    """This class represents a user session,
+        which is used to track user
+    authentication and authorization information.
     """
 
     def __init__(self, *args: list, **kwargs: dict):
         """Initializes a User session instance.
+
+        Parameters:
+        *args (list): Positional arguments to be passed to the parent
+        class constructor.
+        **kwargs (dict): Keyword arguments to be passed to the parent
+        class constructor. The 'user_id' and 'session_id' keys are used
+        to initialize the corresponding instance attributes.
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
